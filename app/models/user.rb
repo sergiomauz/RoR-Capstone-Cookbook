03659a9663e634b3dec14cd431be6c62b83294d2
sergiomauz <sergio.mauz88@gmail.com>
2020-05-27 22:10:19 -0500

    Added User model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :username, presence: true, length: { maximum: 15 }
+
+  validate :minimum_length
+
+  def minimum_length
+    errors.add(:name, '-name- should have 5 characters or more') if name.length < 5
+    errors.add(:username, '-username- should have 3 characters or more') if username.length < 3
+  end  
+end
+  
---
+++ db/migrate/20200528021814_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, limit: 35
+      t.string :username, null: false, unique: true, limit: 15
+      t.string :gravatar_url
+
+      t.timestamps
+    end
+  end
+end
