3358ca69a258f9340ab5117fead32c86ae7623f2
sergiomauz <sergio.mauz88@gmail.com>
2020-05-29 01:17:51 -0500

    Added test for Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,9 @@
 class Ingredient < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 35 }    
+  validates :name, presence: true, length: { minimum: 3, maximum: 35 }
   validate :valid_amount
-  
+
   def valid_amount
-    errors.add(:amount, '-amount- should have a value higher than 0 and less (or equal) than 9999.99') unless amount > 0 || amount <= 9999.99
-  end    
+    errors.add(:amount, '-amount- should be less or equal to 9999.99') if amount > 9999.99
+    errors.add(:amount, '-amount- should be higher than 0') if amount <= 0
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:ingredient) { Ingredient.new(name: 'Carrots', amount: 5.50) }
+
+  it 'Is valid with valid attributes' do
+    expect(ingredient).to be_valid
+  end
+
+  it 'Is not valid if new ingredient -name- has fewer characters than 3' do
+    ingredient.name = 'Ca'
+    expect(ingredient).to_not be_valid
+  end
+
+  it 'Is not valid if new ingredient -name- has more characters than 35' do
+    ingredient.name = 'Carrots planted in the North Pole by Santa Claus'
+    expect(ingredient).to_not be_valid
+  end
+
+  it 'Is not valid if new ingredient -amount- is less or equal to 0' do
+    ingredient.amount = 0
+    ingredient.name = 'Carrots'
+    expect(ingredient).to_not be_valid
+  end
+
+  it 'Is not valid if new ingredient -amount- is higher than 9999.99' do
+    ingredient.amount = 10_000
+    expect(ingredient).to_not be_valid
+  end
 end
