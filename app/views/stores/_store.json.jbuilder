1674af1f60123c91823fa6fc1f4515af36486f4b
sergiomauz <sergio.mauz88@gmail.com>
2020-06-02 17:35:26 -0500

    Scaffolded Store entity

---
+++ app/views/stores/_store.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! store, :id, :name, :created_at, :updated_at
+json.url store_url(store, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index new create]
   resources :groups, only: %i[index show new create]
