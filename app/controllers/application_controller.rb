4e3d27f5e6c4c8abfe70c8bab3684492ab975c15
sergiomauz <sergio.mauz88@gmail.com>
2020-06-03 17:38:40 -0500

    Added constraints for sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :must_be_authenticated
   add_flash_types :error, :success
-
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
     end
+  end
+
+  def must_be_authenticated
+    redirect_to root_path unless current_user
   end
 end
